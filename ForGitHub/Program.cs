f0614a059af85724e038fa4d49c0a2494ecc7bfc
胥明胜 <@>
2017-09-26 15:45:54 +0800

    添加项目文件。

---
+++ ForGitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
